3602b86075b0235504b363cdf84c6403a24bbc3d
kuccho524 <kucchachachanocha@gmail.com>
2021-08-23 19:43:45 +0900

    Initial Commit

---
+++ src/com/ja/JavaBeginner/Practice1.java
@@ -0,0 +1,19 @@
+package com.ja.JavaBeginner;
+
+/*
+ * 縦3横5の長方形の面積を求める
+ */
+
+public class Practice1 {
+	public static void main(String[] args) {
+		
+		// int型を持つ各変数を宣言する
+		int height = 3;
+		int width = 5;
+		int areaReactangular = height * width;
+		
+		// 長方形の面積を出力する
+		System.out.println(areaReactangular);
+	}
+
+}
